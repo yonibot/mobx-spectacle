b24f6891653f1760eba98810bc18dc5efeff048a
Yoni Weisbrod <yoni@artsetters.com>
2016-12-05 03:56:10 +0200

    update index and examples

--- presentation/index.js
+++ presentation/index.js
@@ -276,6 +276,7 @@ disheartening. Because (at least for my team and the specific projects we do), m
             code={require("raw!../assets/my_stores.example")}
             ranges={[ 
               { loc: [9,14] },
+              { loc: [30,31] },
             ]} />
 
           <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
@@ -283,158 +284,72 @@ disheartening. Because (at least for my team and the specific projects we do), m
               <Heading>Common Patterns</Heading>
               <List>
                 <ListItem>Explicit Actions</ListItem>
-                <Appear><ListItem>Domain Stores</ListItem></Appear>
+                <ListItem>Domain Stores</ListItem>
               </List>
             </div>
           </Slide>
 
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={require("raw!../assets/my_stores.example")}
+            ranges={[ 
+              { loc: [0,3] },
+              { loc: [9,13] },
+              { loc: [14,15] },
+              { loc: [20,21] },
+              { loc: [22,24] },
+              { loc: [32,35] },
+            ]} />
+
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <div>
+              <Heading>Common Patterns</Heading>
+              <List>
+                <ListItem>Explicit Actions</ListItem>
+                <ListItem>Domain Stores</ListItem>
+                <ListItem>Automagic Network Requests</ListItem>
+              </List>
+            </div>
+          </Slide>
+
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={require("raw!../assets/my_stores.example")}
+            ranges={[ 
+              { loc: [3,8] }
+            ]} />
 
 
           <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
             <div>
               <Heading>Common Patterns</Heading>
               <List>
-                <Appear><ListItem>Explicit Actions</ListItem></Appear>
-                <Appear><ListItem>Domain Stores</ListItem></Appear>
-                <Appear><ListItem>Automagic Network Requests</ListItem></Appear>
+                <ListItem>Explicit Actions</ListItem>
+                <ListItem>Domain Stores</ListItem>
+                <ListItem>Automagic Network Requests</ListItem>
                 <Appear><ListItem>Simple Routing</ListItem></Appear>
               </List>
-              <Appear><Heading textSize="1.5em" margin="8px 0px 0px">But wait! There's more!</Heading></Appear>
             </div>
           </Slide>
 
-
-
+          <CodeSlide
+            transition={[]}
+            lang="js"
+            code={require("raw!../assets/my_stores.example")}
+            ranges={[ 
+              { loc: [23,28] }
+            ]} />
 
           <Slide transition={["slide", "spin"]} bgColor="primary">
             <Heading caps fit size={1} textColor="tertiary">
               Thank you!
             </Heading>
-          </Slide>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Dead-Simple MobX
-            </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-          </Slide>
-
-
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
+            <br /><br /><br />
+            <Text textSize="1em" textColor="tertiary">Yoni Weisbrod</Text>
+            <Text textSize="1em">Twitter: <Link src="https://twitter.com/yoniweisbrod">@yoniweisbrod</Link></Text>
+            <Text textSize="1em">GitHub: <Link src="https://github.com/yonibot">@yonibot</Link></Text>
           </Slide>
         </Deck>
       </Spectacle>
